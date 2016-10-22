6786c11814bbf1bf124e8288c43509e854499056
vicky <vkirtsou@hotmail.com>
2016-10-22 20:42:04 +0300

    1st page: added logo and margins, rearranged layouts, Next button loads 2nd page, with 1 table + 2 accordion tabs, back button goes to 1st page

--- ScheduleMe/src/main/java/com/example/ScheduleMe/MyUI.java
+++ ScheduleMe/src/main/java/com/example/ScheduleMe/MyUI.java
@@ -7,9 +7,15 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.server.ExternalResource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Table.Align;
+import com.vaadin.ui.themes.ValoTheme;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -21,22 +27,42 @@ import com.vaadin.ui.*;
 @Theme("mytheme")
 public class MyUI extends UI {
 	
-    private List<String> facultyNames = new ArrayList<String>();
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private List<String> facultyNames = new ArrayList<String>();
     private List<String> degreeNames = new ArrayList<String>();
-    //private List<String> degreeNamesMed = new ArrayList<String>();
-    //private List<String> degreeNamesEdu = new ArrayList<String>();
-    
+    private int facultySize = 3;		// static implementation, TODO: change when/if we have database
+    final VerticalLayout layout = new VerticalLayout();
+	private VerticalLayout main = MainLayout();
+	private VerticalLayout selection = SelectionLayout();
+	private VerticalLayout courseSelect = CourseSelectLayout();
+	
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        int facultySize = 3;		// static implementation, TODO: change when/if we have database
-
+        
+        //VerticalLayout selection = SelectionLayout();
+        // add the components to the layout:
+        //VerticalLayout main = MainLayout();
+        //MainLayoutC main = new MainLayoutC(selection);
+        layout.addComponents(main, selection);  
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        setContent(layout);
+        
+    }
+    
+    private VerticalLayout SelectionLayout() {	//---- page1 ----\\
+    	
+    	VerticalLayout l1 = new VerticalLayout();
+    	VerticalLayout l2 = new VerticalLayout();
         NativeSelect facultySelection = new NativeSelect("Select your faculty:");  
         NativeSelect degreeSelection = new NativeSelect("Select your degree:");
         OptionGroup periodOption = new OptionGroup("Select period:");
-        Button nextButton = new Button("Next");        
-        
-        addFacultyNames();	
+    	Button buttonNext;
+    	
+        addFacultyNames();      
         
         for (int i = 0; i < facultySize; i++) {
         	facultySelection.addItem(i);
@@ -45,7 +71,10 @@ public class MyUI extends UI {
         // so that the blank option cannot be chosen
         facultySelection.setNullSelectionAllowed(false);
         degreeSelection.setNullSelectionAllowed(false);
-
+        
+        //degreeSelection.setWidth("360");
+        //degreeSelection.
+        
         // event listener for when we select a faculty to show only the appropriate degrees
         facultySelection.addValueChangeListener(e -> {
         	degreeSelection.removeAllItems();			// clean the default value
@@ -55,20 +84,132 @@ public class MyUI extends UI {
         		degreeSelection.addItem(i);
         		degreeSelection.setItemCaption(i, degreeNames.get(i));
         	}
+        	
         });
 
+    	// setup the period selection:
         for (int i = 0; i < 2; i++) {
         	periodOption.addItem(i);
         	int x=i+1;		// don't ask, lol :/
         	periodOption.setItemCaption(i, "Period " + x);
         }
-        periodOption.setValue(0);
+        periodOption.setValue(0);		// pre-assigned: period 1
         
-        layout.addComponents(facultySelection, degreeSelection, periodOption, nextButton);
-        layout.setMargin(true);
-        layout.setSpacing(true);
+        // setup the button:
+        buttonNext = new Button("Next", new Button.ClickListener() {
+        	// button listener that loads next page
+			@Override
+			public void buttonClick(ClickEvent event) {	
+				//if (!facultySelection.isEmpty() && !degreeSelection.isEmpty()) {		// TODO: Uncomment+ for field checking
+					layout.removeAllComponents();
+					layout.addComponents(main, courseSelect);
+					setContent(layout);	
+				//}																		// TODO: Uncomment+ for field checking
+			}
+		});         
+        //buttonNext.setEnabled(false);													// TODO: Uncomment+ for field checking  
+        //degreeSelection.addValueChangeListener(e -> buttonNext.setEnabled(true));      // TODO: Uncomment+ for field checking  
+        buttonNext.setClickShortcut(ShortcutAction.KeyCode.ENTER);
+        l1.addComponents(facultySelection, degreeSelection, periodOption);
+        l1.setSpacing(true);
+        l1.setMargin(true);
+        l2.addComponents(l1, buttonNext);
+    	return l2;
+    }
+    
+    private VerticalLayout CourseSelectLayout() {	//---- page2 ----\\
+    	VerticalLayout l1 = new VerticalLayout();
+    	VerticalLayout l2 = new VerticalLayout();
+        VerticalLayout selectedCoursesLayout = new VerticalLayout();
+        VerticalLayout coursesLayout = new VerticalLayout();     
+        final Table scheduleTable = new Table();
+        final Accordion courseAccordion = new Accordion();
+        Button buttonBack;
         
-        setContent(layout);
+        // setup the table:
+        String[] days = new String[] {"", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String[] hours = new String[] {"", "8-10", "10-12", "12-14", "14-16", "16-18", "18-20"};
+       //scheduleTable.setSizeFull();
+        scheduleTable.setColumnHeaders();
+        for (int i = 0; i < 6; i++) {		// set the headers
+        	scheduleTable.addContainerProperty(days[i], String.class, null);
+        	scheduleTable.setColumnAlignment(i, Align.CENTER);
+        }
+        for (int i = 0; i < 7; i++) {
+        	
+        }
+        scheduleTable.addItem(new Object[]{"8-10", "asd"}, 0);	// not working????
+        scheduleTable.addItem(new Object[]{"10-12", "dsf"}, 2);
+        
+        // setup the accordion:
+        courseAccordion.setHeight(100.0f, Unit.PERCENTAGE);
+        courseAccordion.addTab(selectedCoursesLayout, "Tap to see your selected courses!");
+        courseAccordion.addTab(coursesLayout, "Tap to see the list of courses");
+        
+        //layout.addComponent(MainLayout());
+
+        
+        // setup the back button:
+        buttonBack = new Button("Back", new Button.ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {			// go back to previous "page", page1		
+				layout.removeAllComponents();
+				layout.addComponents(main, selection);
+				layout.setMargin(true);
+				layout.setSpacing(true);
+				setContent(layout);
+			}
+		});
+        l1.addComponents(scheduleTable, courseAccordion);
+        l2.setSpacing(true);
+        l2.addComponents(l1, buttonBack);
+        return l2;
+    }
+    
+    private VerticalLayout MainLayout() {	//==== UTU logo + app name ====\\
+    	final VerticalLayout main = new VerticalLayout();
+    	final HorizontalLayout header = new HorizontalLayout();
+    	final VerticalLayout middle = new VerticalLayout();
+    	final HorizontalLayout footer = new HorizontalLayout();
+    	
+    	
+    	Button buttonBack = new Button("Back");
+    	Button buttonNext = new Button("Next");
+    	
+    	Label label = new Label("Schedule Me!");
+    	label.addStyleName(ValoTheme.LABEL_LARGE);
+    	label.addStyleName(ValoTheme.LABEL_BOLD);
+    	label.addStyleName(ValoTheme.LABEL_H1);
+    	label.addStyleName(ValoTheme.LABEL_HUGE);
+    	Image headerImage = new Image();
+    	final ExternalResource externalResource = new ExternalResource("http://www.utu.fi/_LAYOUTS/Neoxen/UTUInternet/Styles/utu_logo.jpg");	
+    	
+    	headerImage.setSource(externalResource);
+    	//headerImage.setSource(new ThemeResource("utu_logo.jpg"));
+    	headerImage.setVisible(true);
+    	
+    	//header.setMargin(true);
+    	header.setSpacing(true);
+    	header.setMargin(true);
+    	header.setSizeFull();
+    	header.addComponents(headerImage, label);
+    	header.setComponentAlignment(label, Alignment.MIDDLE_RIGHT);
+    	
+    	middle.setSpacing(true);
+    	middle.setMargin(true);
+    	
+    	footer.setSpacing(true);
+    	footer.setSizeFull();
+    	footer.addComponents(buttonBack, buttonNext);
+    	footer.setComponentAlignment(buttonBack, Alignment.MIDDLE_LEFT);
+    	footer.setComponentAlignment(buttonNext, Alignment.MIDDLE_RIGHT);
+    	
+    	main.addComponents(header);
+    	//header.setSpacing(true);
+    	//header.setMargin(true);
+    	return main;
+    	
+
     }
     
     // For static implementation of faculty names
@@ -78,28 +219,27 @@ public class MyUI extends UI {
         facultyNames.add("Faculty of Education");
     }
     
-    /// For static implementation: degree == 1 corresponds to (some) of the degrees of Mathematics faculty, 2 is Medicine and 3 is education.
+    /// For static implementation: degree == 0 corresponds to (some) of the degrees of Mathematics faculty, 1 is Medicine and 2 is education.
     private void addDegreeNames(int degree) {
     	switch (degree) {
-    	case 0:
+    	case 0:		// math
     		degreeNames.add("Master's Degree Programme in Information Security and Cryptography ");
         	degreeNames.add("Master's Degree Program in Materials Science");
         	degreeNames.add(" Master's Degree Programme in Bioinformatics");
         	degreeNames.add("Master's Degree Programme in Embedded Computing");
         	break;
-    	case 1:
+    	case 1:		// Med
         	degreeNames.add("Neuroscience study program");
         	degreeNames.add("International Career in Health Sciences");
         	break;
-    	case 2:
+    	case 2:		// edu
         	degreeNames.add("Master's Degree Programme in Learning, Learning Environments and Educational Systems");
         	degreeNames.add("Doctor of Philosophy (in Education)");
         	break;
-    	default:
+    	default:		// otherwise
     		degreeNames.add("error! :(");
     		break;
-    	}	
-    	
+    	}		
     }
 
     
@@ -108,3 +248,52 @@ public class MyUI extends UI {
     public static class MyUIServlet extends VaadinServlet {
     }
 }
+
+class MainLayoutC extends VerticalLayout {
+
+    public MainLayoutC(VerticalLayout middle) {	//==== page3 ====\\
+    	final VerticalLayout main = new VerticalLayout();
+    	final HorizontalLayout header = new HorizontalLayout();
+    	//final VerticalLayout middle = new VerticalLayout();
+    	final HorizontalLayout footer = new HorizontalLayout();
+    	
+    	
+    	Button buttonBack = new Button("Back");
+    	Button buttonNext = new Button("Next");
+    	
+    	Label label = new Label("Schedule Me!");
+    	label.addStyleName(ValoTheme.LABEL_LARGE);
+    	label.addStyleName(ValoTheme.LABEL_BOLD);
+    	label.addStyleName(ValoTheme.LABEL_H1);
+    	label.addStyleName(ValoTheme.LABEL_HUGE);
+    	
+    	Image headerImage = new Image();
+    	final ExternalResource externalResource = new ExternalResource("http://www.utu.fi/_LAYOUTS/Neoxen/UTUInternet/Styles/utu_logo.jpg");	
+    	
+    	headerImage.setSource(externalResource);
+    	//headerImage.setSource(new ThemeResource("utu_logo.jpg"));
+    	headerImage.setVisible(true);
+    	
+    	//header.setMargin(true);
+    	header.setSpacing(true);
+    	header.setMargin(true);
+    	header.setSizeFull();
+    	header.addComponents(headerImage, label);
+    	header.setComponentAlignment(label, Alignment.MIDDLE_RIGHT);
+    	
+    	middle.setSpacing(true);
+    	middle.setMargin(true);
+    	
+    	footer.setSpacing(true);
+    	footer.setSizeFull();
+    	footer.addComponents(buttonBack, buttonNext);
+    	footer.setComponentAlignment(buttonBack, Alignment.MIDDLE_LEFT);
+    	footer.setComponentAlignment(buttonNext, Alignment.MIDDLE_RIGHT);
+    	
+    	main.addComponents(header, middle);
+    	//header.setSpacing(true);
+    	//header.setMargin(true);
+    	//return main;
+    }
+}
+    
