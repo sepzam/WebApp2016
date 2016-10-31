8a76d2be9f4c62ef368b4db8c961b0e788aa036e
Sepid-PC\Sepid <sepid.zamanizadeh@gmail.com>
2016-10-31 18:17:17 +0200

    Problem with listener is fixed

--- ScheduleMe/src/main/java/com/example/ScheduleMe/Database.java
+++ ScheduleMe/src/main/java/com/example/ScheduleMe/Database.java
@@ -192,7 +192,7 @@ final class Database extends Window {
 
 						} else if(MyUI.degree==0){
 
-							if(d.equals("BIO") || d.equals("L"))
+							if(d.equals("BIO") || d.equals("L")){
 								
 							grid.addRow(((Node)textCCList.item(0)).getNodeValue().trim(),((Node)textCNList.item(0)).getNodeValue().trim(),((Node)textLECList.item(0)).getNodeValue().trim(), ((Node)textCREList.item(0)).getNodeValue().trim()); // Just to test the apperance in Grid!
 							lectureDays.add(((Node)textSD1List.item(0)).getNodeValue().trim());
@@ -205,9 +205,10 @@ final class Database extends Window {
 							lectureHours.add(((Node)textST3List.item(0)).getNodeValue().trim().toString());    
 						   	
 					         courses.add(new Course(((Node)textCNList.item(0)).getNodeValue().trim(), lectureDays, lectureHours));
-
+							}
 						} else if(MyUI.degree==1){
 							if(d.equals("NSS") || d.equals("CDS") || d.equals("ISS") || d.equals("L") || d.equals("GD"))
+							{
 							grid.addRow(((Node)textCCList.item(0)).getNodeValue().trim(),((Node)textCNList.item(0)).getNodeValue().trim(),((Node)textLECList.item(0)).getNodeValue().trim(), ((Node)textCREList.item(0)).getNodeValue().trim()); // Just to test the apperance in Grid!
 							lectureDays.add(((Node)textSD1List.item(0)).getNodeValue().trim());
 					    	lectureDays.add(((Node)textSD2List.item(0)).getNodeValue().trim().toString());
@@ -219,9 +220,10 @@ final class Database extends Window {
 							lectureHours.add(((Node)textST3List.item(0)).getNodeValue().trim().toString());    
 						   	
 					         courses.add(new Course(((Node)textCNList.item(0)).getNodeValue().trim(), lectureDays, lectureHours));
-
+							}
 						} else if(MyUI.degree==2){
 							if(d.equals("EC") || d.equals("L"))
+							{
 							grid.addRow(((Node)textCCList.item(0)).getNodeValue().trim(),((Node)textCNList.item(0)).getNodeValue().trim(),((Node)textLECList.item(0)).getNodeValue().trim(), ((Node)textCREList.item(0)).getNodeValue().trim()); // Just to test the apperance in Grid!
 							lectureDays.add(((Node)textSD1List.item(0)).getNodeValue().trim());
 					    	lectureDays.add(((Node)textSD2List.item(0)).getNodeValue().trim().toString());
@@ -233,9 +235,10 @@ final class Database extends Window {
 							lectureHours.add(((Node)textST3List.item(0)).getNodeValue().trim().toString());    
 						   	
 					         courses.add(new Course(((Node)textCNList.item(0)).getNodeValue().trim(), lectureDays, lectureHours));
-
+							}
 						} else if(MyUI.degree==3){
 							if(((Node)textORGList.item(0)).getNodeValue().trim().equals("YES"))
+							{
 							grid.addRow(((Node)textCCList.item(0)).getNodeValue().trim(),((Node)textCNList.item(0)).getNodeValue().trim(),((Node)textLECList.item(0)).getNodeValue().trim(), ((Node)textCREList.item(0)).getNodeValue().trim()); // Just to test the apperance in Grid!
 							lectureDays.add(((Node)textSD1List.item(0)).getNodeValue().trim());
 					    	lectureDays.add(((Node)textSD2List.item(0)).getNodeValue().trim().toString());
@@ -247,7 +250,7 @@ final class Database extends Window {
 							lectureHours.add(((Node)textST3List.item(0)).getNodeValue().trim().toString());    
 						   	
 					         courses.add(new Course(((Node)textCNList.item(0)).getNodeValue().trim(), lectureDays, lectureHours));
-
+							}
 						} 
 						
 						  
